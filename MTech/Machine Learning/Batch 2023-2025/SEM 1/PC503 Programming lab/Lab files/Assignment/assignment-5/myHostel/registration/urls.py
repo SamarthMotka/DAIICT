33d00c9b8b92a0cdbe57da7ad3e6b33d84ac8b4c
Samarth <71966305+SamarthMotka@users.noreply.github.com>
2024-07-26 14:43:07 +0530

    26.7

--- MTech/Machine Learning/Batch 2023-2025/SEM 1/PC503 Programming lab/Lab files/Assignment/assignment-5/myHostel/registration/urls.py
+++
@@ -1,8 +0,0 @@
-# registration/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.user_login, name='login'),
-]
